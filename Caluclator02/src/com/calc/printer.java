2739e5064cbabe50da159d95bfc7cc1bb4044a12
FevenBe <pomimango@gmail.com>
2021-04-17 23:03:27 -0700

    Second calc

---
+++ Caluclator02/src/com/calc/printer.java
@@ -0,0 +1,13 @@
+package com.calc;
+
+public class printer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int age=24;
+String  name="Pomi";
+String city= "Addis Ababa";
+System.out.println(name + " is born in " + city +"."+ "she is " + age +" year old");
+	}
+
+}
